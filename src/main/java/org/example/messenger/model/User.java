9d18daadfd543c6254e9711b90116e21e8df9bed
Andrew Sultanov <vysotnik3@gmail.com>
2025-03-10 14:46:45 +0500

    Этот класс описывает, как будет выглядеть пользователь в системе: у него есть идентификатор (id), имя пользователя (username) и пароль (password).

---
+++ src/main/java/org/example/messenger/model/User.java
@@ -0,0 +1,32 @@
+package org.example.messenger.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @JsonIgnore
+    private String password;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
