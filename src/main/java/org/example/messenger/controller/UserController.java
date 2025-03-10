7a85b53f1881f7c6cb4e6cd2e73865afb8543808
Andrew Sultanov <vysotnik3@gmail.com>
2025-03-10 14:35:22 +0500

    Это нужен для обработки HTTP-запросов, связанных с пользователями.

---
+++ src/main/java/org/example/messenger/controller/UserController.java
@@ -0,0 +1,22 @@
+package org.example.messenger.controller;
+
+
+import org.example.messenger.model.User;
+import org.example.messenger.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+}
