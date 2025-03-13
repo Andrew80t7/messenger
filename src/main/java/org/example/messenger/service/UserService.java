88632c6cc50e3b4fa67d876c5d1af55533f4e29e
Andrew Sultanov <vysotnik3@gmail.com>
2025-03-13 11:28:27 +0500

    Добавил шифрование пароля для пользователя

--- src/main/java/org/example/messenger/service/UserService.java
+++ src/main/java/org/example/messenger/service/UserService.java
@@ -3,7 +3,9 @@ package org.example.messenger.service;
 import org.example.messenger.model.User;
 import org.example.messenger.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.example.messenger.config.SecurityConfig;
 
 @Service
 public class UserService {
@@ -11,10 +13,15 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User registerUser(String username, String password) {
         User user = new User();
         user.setUsername(username);
-        user.setPassword(password); //Добавить шифрование поля
+
+        user.setPassword(passwordEncoder.encode(password));
+//        user.setPassword(password);
 
         return userRepository.save(user);
     }
